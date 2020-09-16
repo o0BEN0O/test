b79a719e3d6902ab4a7c4fa662c8de1e7a102be5
linshubin <857849964@qq.com>
2020-09-16 13:42:32 +0800

    perfect file params rw

--- lan_conf_date.c
+++ lan_conf_date.c
@@ -2,6 +2,9 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define lan_conf_date "/home/ben/ben/4g_router/test/lan_conf_date"
 
@@ -28,7 +31,7 @@ int stringFind(const char *string, const char *dest) {
 	return -1;
 }
 
-int get_value_from_conf(char *file_name,char *key,char *value,char *identifier)
+int get_value_from_file(char *file_name,char *key,char *value,char *identifier)
 {
 	char line_buffer[64]={0};
 	char get_buffer[64]={0};
@@ -53,30 +56,45 @@ int get_value_from_conf(char *file_name,char *key,char *value,char *identifier)
 		if(stringFind(line_buffer,key_buffer)==strlen(key_buffer)){
 			pos -= strlen(line_buffer);
 			pos += strlen(key_buffer);
-			value_len = (strlen(line_buffer)-strlen(key_buffer));/*rm \n*/
+			value_len = (strlen(line_buffer)-strlen(key_buffer));
 			fseek(fp,pos,SEEK_SET);
-			fread(value,1,value_len-1,fp);
+			fread(value,1,value_len-1,fp);/* rm \n */
+			break;
 		}
 	}
 	fclose(fp);
 	return 0;
 }
 
-int set_value_to_conf(char *file_name,char *key,char *value,char *identifier)
+int set_value_to_file(char *file_name,char *key,char *value,char *identifier)
 {
 	char line_buffer[64]={0};
 	char set_buffer[64]={0};
 	char key_buffer[64]={0};
+	
+	char *buf1; /*save before change line*/
+	char *buf2;/*save after change line*/
+	char *total_buf;
+
 	char *clean_buf;
 	int line_len=0;
 	int set_len=0;
+	int total_len=0;
 	int pos=0;
 	int i=0;
+	struct stat file;
+	stat(file_name,&file);
+	int file_size = file.st_size;
 	FILE *fp = fopen(file_name, "r+");
-	if(identifier != NULL)
+	if(identifier != NULL){
 		snprintf(key_buffer,64,"%s%s",key,identifier);
-	else
+		snprintf(set_buffer,64,"%s%s%s\n",key,identifier,value);
+	}
+	else{
 		snprintf(key_buffer,64,"%s",key);
+		snprintf(set_buffer,64,"%s%s\n",key,value);
+	}
+	set_len=strlen(set_buffer);
 	if(fp == NULL)
 	{
 		printf("open error");
@@ -87,11 +105,34 @@ int set_value_to_conf(char *file_name,char *key,char *value,char *identifier)
 		line_len=strlen(line_buffer);
 		pos += line_len;
 		if(stringFind(line_buffer,key_buffer)==strlen(key_buffer)){
+			if(pos-line_len != 0){
+				buf1=(char *)malloc(sizeof(char)*(pos-line_len));
+				//printf("pos %d line_len%d\n",pos,line_len);
+				memset(buf1,0,pos-line_len);
+				fseek(fp,0,SEEK_SET);
+				fread(buf1,1,pos-line_len,fp);
+				//printf("1.%s\n",buf1);
+			}
+
+			buf2=(char *)malloc(sizeof(char)*(file_size-pos));
+			memset(buf2,0,file_size-pos);
+			fseek(fp,pos,SEEK_SET);
+			fread(buf2,1,file_size-pos,fp);
+
+			total_len=file_size-line_len+set_len;
+			total_buf=(char *)malloc(sizeof(char)*(total_len+1));
+			snprintf(total_buf,total_len+1,"%s%s%s",buf1,set_buffer,buf2);
+			//printf("2.%s",buf2);
+			fclose(fp);
+			
+			fp = fopen(file_name, "wt");
+			fwrite(total_buf,1,total_len,fp);
+
+			#if 0
 			pos -= line_len;
 			fseek(fp,pos,SEEK_SET);
 			if(strlen(set_buffer)<line_len){
-				snprintf(set_buffer,64,"%s=%s\n",key,value);
-				set_len=strlen(set_buffer);
+				
 				fwrite(set_buffer,1,set_len,fp);
 				//fprintf(fp,"\n");
 				clean_buf = (char *)malloc(sizeof(char)*(line_len-set_len));
@@ -103,19 +144,23 @@ int set_value_to_conf(char *file_name,char *key,char *value,char *identifier)
 				free(clean_buf);
 				break;
 			}
-			snprintf(set_buffer,64,"%s=%s",key,value);
 			fprintf(fp,"%s",set_buffer);
 			fprintf(fp,"\n");
 			break;
+			#endif
 		}
 	}
 	fclose(fp);
 	return 0;
 }
-void main(void)
+void main(int argc, char **argv)
 {
+	char key[64]={0};
 	char value[64]={0};
-	set_value_to_conf(lan_conf_date,"BOOTMODE","11111","=");
-	get_value_from_conf(lan_conf_date,"BOOTMODE",value,"=");
+	char ret_value[64]={0};
+	strcpy(key,argv[1]);
+	strcpy(value,argv[2]);
+	set_value_to_file(lan_conf_date,key,value,"=");
+	get_value_from_file(lan_conf_date,key,ret_value,"=");
 	printf("%s %d",value,(int)strlen(value));
 }
